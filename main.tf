6e8e8d6bf05584141dc3aecd68f1b5ad638622d3
Kyle Atendido <kristoffer.atendido@gmail>
2022-01-29 10:57:42 +0800

    Added another EC2 instance and changed name of target group attachment

--- main.tf
+++ main.tf
@@ -87,13 +87,13 @@ resource "aws_lb_target_group" "test" {
 
 resource "aws_lb_target_group_attachment" "my-alb-target-group-attachment1" {
   target_group_arn = aws_lb_target_group.test.arn
-  target_id        = aws_instance.ec2_instance_2.id
+  target_id        = aws_instance.ec2_instance.id
   port             = 443
 }
 
-resource "aws_lb_target_group_attachment" "my-alb-target-group-attachment1" {
+resource "aws_lb_target_group_attachment" "my-alb-target-group-attachment2" {
   target_group_arn = aws_lb_target_group.test.arn
-  target_id        = aws_instance.ec2_instance.id
+  target_id        = aws_instance.ec2_instance_2.id
   port             = 443
 }
 
